30a30832ea6484a05a548e61d9aeccb122a2fc38
Tyuiu.OdarenkoAR@outlook.com <odarenkoalina@mail.ru>
2023-10-01 00:33:38 +0500

    Task2

---
+++ Tyuiu.OdarenkoAR_Sprint0.Task2.V0.Test/DataSeviceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.OdarenkoAR_Sprint0.Task2.V0.Lib;
+namespace Tyuiu.OdarenkoAR_Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алина";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Алина", res);
+        }
+    }
+}
---
+++ Tyuiu.OdarenkoAR_Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.OdarenkoAR_Sprint0.Task2.V0.Lib;
+namespace Tyuiu.OdarenkoAR_Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Алина"));
+            Console.ReadKey();
+        }
+    }
+}
